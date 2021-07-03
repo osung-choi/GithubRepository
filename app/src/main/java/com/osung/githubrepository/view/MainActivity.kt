eaf37c86940727720a8bb6f12828700795d248c4
최오성 <osung4792@naver.com>
2021-07-03 22:19:34 +0900

    GithubRepository-4 검색 UI 작업
    1. MainActivity.kt 작업
      - DataBinding, ViewModel 생성
      - lifecycleOwner 지정
      - RecyclerView - Adapter 연결
      - LinearLayoutManager 지정

--- app/src/main/java/com/osung/githubrepository/view/MainActivity.kt
+++ app/src/main/java/com/osung/githubrepository/view/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.osung.githubrepository.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.osung.githubrepository.R
+import com.osung.githubrepository.databinding.ActivityMainBinding
+import com.osung.githubrepository.view.adpater.SearchAdapter
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModel()
+
+    private val adapter by lazy { SearchAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        binding.searchResultList.adapter = adapter
     }
 }
